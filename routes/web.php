38c6c08147ed34d5aaeffbf42b1b2a85a3bca949
Alvaro <avicent.dev@gmail.com>
2026-01-12 12:10:46 +0100

    fix : fix routes files

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\CVConversationController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::prefix('cv')->group(function () {
+    Route::post('/start', [CVConversationController::class, 'start']);
+    Route::post('/answer', [CVConversationController::class, 'answer']);
 });
